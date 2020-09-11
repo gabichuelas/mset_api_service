45a53646ce2d93a7c6ef566f92c382696a066d8d
Gaby Mendez <gmendez90@gmail.com>
2020-09-11 16:51:43 -0400

    Add figaro to gemfile, write test route

--- api_calls.rb
+++ api_calls.rb
@@ -1,6 +1,7 @@
-get '/' do
-  "Hello World!"
-  # faraday call out to medication app
-  # parse response
-  # send response back to view?
+get '/test' do
+  "Hello World"
 end
+
+# faraday calls out to external api's
+# paths are endpoints that must match the ones in rails app
+# what the block returns is what we want to show up in rails app.
