f54d3dab0fb7bf3e3b7dfd185556d8535089dd19
Gaby Mendez <gmendez90@gmail.com>
2020-09-12 23:07:09 -0400

    Write route for /sym_search

--- app/controllers/mset_api_service_app.rb
+++ app/controllers/mset_api_service_app.rb
@@ -1,6 +1,10 @@
 class MsetApiService < Sinatra::Base
 
-  
+  get '/sym_search' do
+    response = FdaService.new.symptom_search(params[:product_ndc])
+    response.body
+  end
+
   get '/med_search' do
     response = FdaService.new.brand_name_search(params[:medication_name])
     response.body
