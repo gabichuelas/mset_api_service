e4a019c7f97361711f2116e48e182a609b1fe3d5
Gaby Mendez <gmendez90@gmail.com>
2020-09-13 01:07:20 -0400

    Create #adverse_reactions_table as an alternative endpoing

--- app/controllers/mset_api_service_app.rb
+++ app/controllers/mset_api_service_app.rb
@@ -1,8 +1,13 @@
 class MsetApiService < Sinatra::Base
 
   get '/sym_search' do
+    # product_labeling: OPEN FDA SEARCH BY PRODUCT_NDC
     response = FdaService.new.symptom_search(params[:product_ndc])
     response.body
+
+    # product_labeling: ADVERSE_EVENTS ENDPOINT
+    # response = FdaService.new.adverse_reactions_table(params[:product_ndc])
+    # response.body
   end
 
   get '/med_search' do
--- app/services/fda_service.rb
+++ app/services/fda_service.rb
@@ -1,5 +1,9 @@
 class FdaService
 
+  def adverse_reactions_table(medication_name)
+    conn.get("drug/label.json?search=adverse_reactions:#{medication_name}")
+  end
+
   def symptom_search(product_ndc)
     conn.get("drug/label.json?search=openfda.product_ndc.exact:#{product_ndc}")
   end
