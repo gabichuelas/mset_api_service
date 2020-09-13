db7ac70b24b019bf3a51b71206fca50d39ece360
Gaby Mendez <gmendez90@gmail.com>
2020-09-12 23:04:08 -0400

    Create symptom_search

--- app/services/fda_service.rb
+++ app/services/fda_service.rb
@@ -1,8 +1,11 @@
 class FdaService
 
-  def brand_name_search(params)
-    conn.get("drug/ndc.json?search=brand_name_base:#{params}&limit=10")
-    # json_parse(response)
+  def symptom_search(product_ndc)
+    conn.get("drug/label.json?search=openfda.product_ndc.exact:#{product_ndc}")
+  end
+
+  def brand_name_search(medication_name)
+    conn.get("drug/ndc.json?search=brand_name_base:#{medication_name}&limit=10")
   end
 
   private
