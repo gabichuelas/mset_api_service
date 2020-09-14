79f15736d82035f92b68a20532ed1f94bdb783dc
Gaby Mendez <gmendez90@gmail.com>
2020-09-14 17:53:52 -0400

    Cleanup code in vcr spec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -112,9 +112,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.filter_sensitive_data('<MOVIE_DB_API_KEY_V3>') { ENV['MOVIE_DB_API_KEY_V3'] }
-  # config.filter_sensitive_data('<GOOGLE_CLIENT_SECRET>') { ENV['GOOGLE_CLIENT_SECRET'] }
-  # config.default_cassette_options = { re_record_interval: 3.days }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
 end
