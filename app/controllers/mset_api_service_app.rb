05f91e8639567e60fd5bef534e5388510bc61a02
Gaby Mendez <gmendez90@gmail.com>
2020-09-18 15:16:20 -0400

    Feedback comments from ian

--- app/controllers/mset_api_service_app.rb
+++ app/controllers/mset_api_service_app.rb
@@ -1,4 +1,7 @@
 class MsetApiService < Sinatra::Base
+# set headers for responses to send back as json
+# maybe a before_action / before_block?
+# response.headers["Content-Type"] = application/json
 
   get '/sym_search' do
     response = FdaService.new.symptom_search(params[:product_ndc])
@@ -7,6 +10,7 @@ class MsetApiService < Sinatra::Base
 
   get '/med_search' do
     response = FdaService.new.brand_name_search(params[:medication_name])
+    response.headers["Content-Type"] = "application/json"
     response.body
   end
 end
